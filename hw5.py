eb4618d6b3392cc49c3066567e612c7f27c9179d
106070010 <37007615+smpss94236@users.noreply.github.com>
2018-04-24 18:05:19 +0800

    first

--- hw5.py
+++ hw5.py
@@ -1,46 +1,60 @@
-# -*- coding: utf-8 -*-
-import ...
+import string
+
+
+def main(filename):import string
 
 
 def main(filename):
     # read file into lines
-    lines = ...
+    #lines = open('i_have_a_dream.txt').readlines()
+    lines = open(filename).readlines()
 
-    # declare a word list
     all_words = []
 
     # extract all words from lines
     for line in lines:
-        # split a line of text into a list words
-        # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
-        # check the format of words and append it to "all_words" list
+        line = line.strip()
+        words = line.split()
+    
         for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
+            word = word.strip(string.punctuation)
             if word:
-                # append the word to "all_words" list
-                all_words...
+                all_words.append(word)
+
 
     # compute word count from all_words
-    counter = ...
+    from collections import Counter
+
+    word_counter = Counter(all_words)
+
+    #word_counter.most_common() #
+
+    import csv
+    csv_file = open('wordcount.csv','w')
+
+
+    with open('wordcount.csv','w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+    
+        writer.writerow(['word', 'count'])
+    
+    #for word, count in word_counter.most_common():
+        #writer.writerow(['word', 'count'])
+        writer.writerows(word_counter.most_common())
+    #微軟的bug
+
+    import json
+
+    json.dump(word_counter.most_common(),open('wordcount.json', 'w'))
+
+
+    import pickle
+
+    pickle.dump(word_counter.most_common(),open('wordcount.pkl', 'wb'))
+
 
     # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
-    with open(...) as csv_file:
-        # create a csv writer from a file object (or descriptor)
-        writer = ...
-        # write table head
-        writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
-        writer.writerows(...)
-
+   
     # dump to a json file named "wordcount.json"
     ...
 
